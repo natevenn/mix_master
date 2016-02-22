c1512dc79eb9d24b84924b9a7d32d1b6bd6a5eab
Nathan Venn <natevenn@gmail.com>
2016-02-22 07:54:13 -0700

    add controller test for sad path

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,5 @@ class Artist < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :image_path, presence: true
 
-  has_many :songs
+  has_many :songs, dependent: :destroy
 end
